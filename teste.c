ec87b4c69aa65bf863f7f818e2ed98407391a908
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-02 20:08:59 -0300

    atatt

--- teste.c
+++ teste.c
@@ -1,42 +1,12 @@
 #include "src/one.c"
 MakTubNameskace mak;
 
-void joga_minecraft(MakTub *root_obj){
-    printf("e jogo minecraft");
-
-}
-
-void joga_poker(MakTub *root_obj){
-    printf("e jogo poker");
-}
-
-void print_mateus(MakTub *root_obj){
-    printf("mateus");
-    MakTubeGenerationAction *action = mak.newGenerationAction(root_obj);
-    mak.actions.subscribe_function(action,0.5, joga_poker);
-    mak.actions.subscribe_function(action,0.5, joga_minecraft);
-    mak.actions.perform(action);
-}
-void print_danilo(MakTub *root_obj){
-    printf("danilo ");
-}
-void print_pocket(MakTub *root_obj){
-    printf("pocket ");
-    MakTubeGenerationAction *action = mak.newGenerationAction(root_obj);
-    mak.actions.subscribe_function(action,0.5, joga_poker);
-    mak.actions.subscribe_function(action,0.5, joga_minecraft);
-   mak.actions.perform(action);
-}
 
 int main(){
-    mak =newMakTubNameskace();
-    MakTub * test = mak.newMakTub("aaa232m3i4sss");
-
-    printf("meu nome é ");
-    MakTubeGenerationAction *action = mak.newGenerationAction(test);
-    mak.actions.subscribe_function(action,0.33, print_danilo);
-    mak.actions.subscribe_function(action,0.33, print_mateus);
-    mak.actions.subscribe_function(action,0.33, print_pocket);
-    mak.actions.perform(action);
+   mak =newMakTubNameskace();
+   MakTub * test = mak.newMakTub("aaa232m3i4sss");
+   const char *t[] = {"aaa","aaa","ddd"};
+    char *escolhido  = Maktub_generate_choice(test,(void**)t,sizeof(t)/sizeof(char*));
+    printf("%s",escolhido);
    MakTub_free(test);
 }
