7d3d49af3d5ba802bcb7aa229d8962d04e99894c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-05 22:20:11 -0300

    att

---
+++ src/src_dependencies/dep_declare.dependencies.h
@@ -0,0 +1,6 @@
+
+
+#if !defined(UniversalGarbage_fdeclare) && !defined(MAKTUB_MOCK_UNIVERSAL_GARBAGE)
+#include "../../dependencies/UniversalGarbage.h"
+#define MAKTUB_UNIVERSAL_GARBAGE_DECLARATED
+#endif
