70974f503bf6ed7b4d2566c059e129b03f071893
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-05 23:16:56 -0300

    att

--- src/consts/macros.algo.h
+++ src/consts/macros.algo.h
@@ -1,10 +1,4 @@
 
-//silver_chain_scope_start
-//DONT MODIFY THIS COMMENT
-//this import is computationally generated
-//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
-#include "../imports/imports.dep_declare.h"
-//silver_chain_scope_end
 #define MAKTUBE_SUFFLE 7
 #define MAKTUBE_FACTOR 1.55
 #define MAKTUBE_ONE_MILLION 1000000
--- src/consts/macros.sequencials.h
+++ src/consts/macros.sequencials.h
@@ -1,10 +1,4 @@
 
-//silver_chain_scope_start
-//DONT MODIFY THIS COMMENT
-//this import is computationally generated
-//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
-#include "../imports/imports.dep_declare.h"
-//silver_chain_scope_end
 #define MAKTUBE_NUMBERS  "0123456789"
 
 #define MAKTUBE_NUMBERS        "0123456789"
