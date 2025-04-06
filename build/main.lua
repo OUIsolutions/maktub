90da1a840a6e874acb7f7a8be1a10f5b78e9db98
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-05 22:06:56 -0300

    att

---
+++ build/main.lua
@@ -0,0 +1,60 @@
+function main()
+    install_dependencies()
+    darwin.silverchain.generate({
+        src = "src",
+        tags = { "dep_declare", "macros", "types","consts", "fdeclare","globals", "dep_define","fdefine" },
+        project_short_cut = "Maktub",
+        implement_main = false
+    })
+
+    if darwin.argv.one_of_args_exist("only_silverchain") then
+        return 
+    end 
+
+    local MAX_CONNTENT = darwin.camalgamator.ONE_MB * 20
+    local MAX_RECURSION = 100
+
+    local lincense = "/* " .. darwin.dtw.load_file("LICENSE") .. " */ \n"
+
+    local onefile = darwin.camalgamator.generate_amalgamation("src/one.c", MAX_CONNTENT, MAX_RECURSION)
+    onefile = lincense .. onefile
+
+    darwin.dtw.write_file("release/MaktubOne.c", onefile)
+
+    local only_declare = darwin.camalgamator.generate_amalgamation("src/imports/imports.fdeclare.h", MAX_CONNTENT,
+    MAX_RECURSION)
+    only_declare = lincense .. only_declare
+    darwin.dtw.write_file("release/Maktub.h", only_declare)
+
+
+    local only_definition = darwin.camalgamator.generate_amalgamation_with_callback("src/imports/imports.fdefine.h",
+        function(import, path)  
+   
+            
+
+            if darwin.dtw.ends_with(import,"UniversalGarbage.h") then
+                return "dont-include"
+            end
+
+            if darwin.dtw.ends_with(path,"imports.fdeclare.h") then
+                    return "dont-include"
+            end
+
+
+
+            return "include-once"
+        end,
+        
+        MAX_CONNTENT,
+        MAX_RECURSION
+    )
+
+    only_definition = '#include "Maktub.h"\n' .. only_definition
+
+    only_definition = lincense .. only_definition
+
+    darwin.dtw.write_file("release/Maktub.c", only_definition)
+
+    os.execute("zip -r release/Maktub.zip dependencies src build")
+
+end
